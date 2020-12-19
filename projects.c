a7666265ea7907890dc86bdaa3998bb4b94c4df2
oussamachah2020 <otaksam213@gmail.com>
2020-12-19 12:16:37 +0100

    projacts.c

---
+++ projects.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h> //for sleep() function
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main()
+{
+  int hour,minute,second;
+  hour = minute = second = 0;
+
+  while(1)
+  {
+
+    system("clear"); //to clear the output
+
+    printf("\t\t\t\t %02d : %02d : %02d",hour,minute,second);
+
+    fflush(stdout);
+
+    second++;
+
+    if(second==60)
+    {
+      minute+=1;
+      second=0;
+    }
+    if(minute==60)
+    {
+      hour+=1;
+      minute=0;
+    }
+    if(hour==24)
+    {
+      second+=1;
+      minute=0;
+      hour=0;
+    }
+    sleep(1); 
+  }
+  return 0;
+}
